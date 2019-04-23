b93bfe1262465798dad5d081cb24b0cc53306d9f
zhaoqiangxu <zhaoqiangxu8@gmail.com>
2019-04-23 20:58:39 +0800

    改动

--- src/com/atguigu/git/TestGit.java
+++ src/com/atguigu/git/TestGit.java
@@ -1,5 +1,7 @@
 package com.atguigu.git;
 
 public class TestGit {
-
+	public static void main(String[] args) {
+		System.out.println("6666666");
+	}
 }
---
+++ src/com/atguigu/service/Employee.java
@@ -0,0 +1,7 @@
+package com.atguigu.service;
+
+public class Employee {
+public static void main(String[] args) {
+	System.out.println("飞起来了！");
+}
+}
